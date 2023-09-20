37949fdee22ac415bc1565e04d9ee16218df5f1a
DimamonDESKTOP <dimamon02@gmail.com>
2023-09-20 17:01:24 +0300

    4

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-print('Nothing')
-print('will do')
-print('unless you do')
+from colorama import Fore, Style
+
+a = "Anyone who"
+b = "stops"
+c = "learning is old,"
+d = 'whether at twenty or eighty'
+
+print(Fore.YELLOW + a)
+print(b)
+print(c)
+print(d)
+print(Style.RESET_ALL + "John Lennon")
